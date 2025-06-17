52072f8a0d7df7b4aaf0f4e1a8d5d49a97d47620
parsonsmatt <parsonsmatt@gmail.com>
2025-06-17 16:30:10 -0600

    commentary

--- src/StmContainers/Trie.hs
+++ src/StmContainers/Trie.hs
@@ -1,3 +1,10 @@
+-- | This module contains the interface for working with a @'Trie' k v@
+-- a concurrent map of @[k]@ key paths to @v@ values.
+--
+-- This library is based on the "StmContainers.Map" module, and should
+-- enjoy the performance benefits of that type.
+--
+-- @since 0.0.1.0
 module StmContainers.Trie
     ( -- * Datatype
       Trie
